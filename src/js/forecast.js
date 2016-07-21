2b36cfdd3090511a187e47922296e97501b0f22b
Marcus Fábio Salviano <marcussalviano@gmail.com>
2016-07-21 13:16:47 -0300

    Alteracoes de layout

---
+++ src/js/forecast.js
@@ -0,0 +1,12 @@
+
+var app = angular.module("AppForecast", ['ngMaterial']);
+
+app.controller("ForecastController", function($scope, $http) {
+    $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?id=3399415&APPID=bc260d742ce6d1a6ed3246f79d8886f8&units=metric").
+    success(function(data) {
+        console.log(data);
+        $scope.location = data;
+        $scope.weatherImg = "http://openweathermap.org/img/w/" + $scope.location.list[0].weather[0].icon + ".png";
+        $scope.days=$scope.location.list;
+    });
+});
